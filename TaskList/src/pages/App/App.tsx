40f3d2ee556f28a2c899869246c26132abdf48d6
caiosev <kaiosev9@gmail.com>
2022-03-29 08:10:46 -0300

    tasklist exibindo item nao estilizado

---
+++ TaskList/src/pages/App/App.tsx
@@ -0,0 +1,29 @@
+import * as C from './App.styles'
+import {useState} from 'react'
+import {Item} from '../../types/item'
+export const App = () =>{
+
+  const [list, setList] = useState<Item[]>([
+    {
+      id: 1,name:'a',done:false
+    },
+    {
+      id: 2,name:'n',done:false
+    }
+  ])
+
+
+  return(
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de tarefas</C.Header>
+        {/* Add tarefa*/}
+
+        {list.map((item,index) =>(
+         item.name
+         ))}
+
+      </C.Area>
+    </C.Container>
+  )
+}
