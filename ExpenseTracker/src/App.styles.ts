7fdfe85c198f6d83250d6b09c5ed22032401c71d
caiosev <kaiosev9@gmail.com>
2022-04-10 16:21:01 -0300

    estrutura base

---
+++ ExpenseTracker/src/App.styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+`
+
+
+export const Header = styled.header`
+    background-color: darkblue;
+    height: 150px;
+    text-align: center;
+`
+
+export const HeaderText = styled.h1`
+    margin: 0;
+    padding: 0;
+    color: #fff;
+    padding-top: 40px;
+
+`
+
+export const Body = styled.main`
+    margin:  auto;
+    max-width: 1000px;
+    margin-bottom: 30px;
+`
---
+++ ExpenseTracker/src/App.tsx
@@ -0,0 +1,18 @@
+import * as S from './App.styles'
+
+const App = () =>{
+  return(
+    <S.Container>
+      <S.Header>
+        <S.HeaderText>Sistema Finaneiro</S.HeaderText>
+      </S.Header>
+      <S.Body>
+        {/* Area de Info */}
+        {/* Area de Insert */}
+        {/* Tabela */}
+      </S.Body>
+    </S.Container>
+  );
+}
+
+export default App
