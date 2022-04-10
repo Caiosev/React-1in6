89016b04191d0390e3beb7e0790fffef2d14e74a
caiosev <kaiosev9@gmail.com>
2022-04-10 17:01:56 -0300

    filterListByMonth

--- ExpenseTracker/src/App.tsx
+++ ExpenseTracker/src/App.tsx
@@ -3,13 +3,20 @@ import {Item} from './types/Item'
 import {Category} from './types/Category'
 import {categories} from './data/categories'
 import {items} from './data/items'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {getCurrentMonth} from './helpers/dateFilter'
 
 const App = () =>{
 
   const [list,setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([])
   const [currentMonth,setCurrentMonth] = useState(getCurrentMonth());
+
+
+  useEffect(()=>{
+    
+  },[list,currentMonth])
+
 
   return(
     <S.Container>
