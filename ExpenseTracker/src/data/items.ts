c12639b6bcad9fc687eabaa56f6a37eb530a3da8
caiosev <kaiosev9@gmail.com>
2022-04-10 16:34:38 -0300

    tipagem and data

---
+++ ExpenseTracker/src/data/items.ts
@@ -0,0 +1,28 @@
+import {Item} from '../types/Item'
+
+export const items:Item[] = [
+    {
+        date:new Date(2022,3,10),
+        category:'food',
+        title:'McDonalds',
+        value:32.50
+    },
+    {
+        date:new Date(2022,3,10),
+        category:'food',
+        title:'Burguer Kink',
+        value:25.30
+    },
+    {
+        date:new Date(2022,3,11),
+        category:'rent',
+        title:'Aluguel Apt',
+        value:2000
+    },
+    {
+        date:new Date(2022,4,15),
+        category:'salary',
+        title:'Salario',
+        value:6300
+    }  
+];
