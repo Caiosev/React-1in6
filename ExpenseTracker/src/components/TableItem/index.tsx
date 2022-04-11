48a4a8927d63436226c587da86a8ecc00ce37e1b
caiosev <kaiosev9@gmail.com>
2022-04-11 00:13:33 -0300

    color category

--- ExpenseTracker/src/components/TableItem/index.tsx
+++ ExpenseTracker/src/components/TableItem/index.tsx
@@ -1,6 +1,7 @@
 import * as S from './styles'
 import {Item} from '../../types/Item'
 import {formatDate} from '../../helpers/dateFilter'
+import {categories} from '../../data/categories'
 type Props ={
     item:Item
 }
@@ -9,9 +10,13 @@ export const TableItem = ({item}:Props) =>{
     return(
         <S.Tableline>
             <S.TableColumn>{formatDate(item.date)}</S.TableColumn>
-            <S.TableColumn>{item.category}</S.TableColumn>
+            <S.TableColumn>
+                <S.Category color={categories[item.category].color}>
+                    {categories[item.category].title}
+                </S.Category>
+                </S.TableColumn>
             <S.TableColumn>{item.title}</S.TableColumn>
-            <S.TableColumn>{item.value}</S.TableColumn>
+            <S.TableColumn>{`R$${item.value}`}</S.TableColumn>
         </S.Tableline>
     )
 }
