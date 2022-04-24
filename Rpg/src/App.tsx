3255137c8c58a7a1f004deacce96b405ce753d4b
caiosev <kaiosev9@gmail.com>
2022-04-24 01:44:04 -0300

    map div

---
+++ Rpg/src/App.tsx
@@ -0,0 +1,15 @@
+import * as S from './App.styles'
+
+const App = () =>{
+
+
+  return(
+    <S.Container>
+      <S.Map>
+        
+      </S.Map>
+    </S.Container>
+  )
+}
+
+export default App;
