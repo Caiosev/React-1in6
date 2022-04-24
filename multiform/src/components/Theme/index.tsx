00ca1ad5f1b321bce170db07477557b09024f7b7
caiosev <kaiosev9@gmail.com>
2022-04-24 20:27:49 -0300

    done

--- multiform/src/components/Theme/index.tsx
+++ multiform/src/components/Theme/index.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react'
 import { Header } from '../Header'
+import { SidebarItem } from '../SideBarItem'
+import {useForm} from '../../contexts/FormContext'
 import * as S from './styles'
 
 type Props = {
@@ -7,13 +9,19 @@ type Props = {
 }
 
 export const Theme=({children}:Props)=>{
+
+    const {state} = useForm()
+
+
     return(
         <S.Container>
             <S.Area>
                 <Header />
                 <S.Steps>
                     <S.Sidebar>
-                        ... 
+                        <SidebarItem title="Pessoal" desc="Se Identifique" icon="profile" path='/' active={state.currentStep===1}/>
+                        <SidebarItem title="Profissional" desc="Seu nivel" icon="book" path='/step2' active={state.currentStep===2}/>
+                        <SidebarItem title="Contato" desc="Como te achar" icon="mail" path='/step3' active={state.currentStep===3}/>
                     </S.Sidebar>
                     <S.Page>
                         {children}
